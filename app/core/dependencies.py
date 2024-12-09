9bb9013ac01b7cb27371a53e41311f36cd5db214
kofolmarko <kofolmarko00@gmail.com>
2024-12-09 21:15:13 +0100

    fix: :bug: previous change did not solve issue, reverting

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,5 +1,5 @@
 from supabase import create_client, Client
-from app.core.config import settings
+from core.config import settings
 
 def get_supabase_client() -> Client:
     return create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.routes import router
+from api.routes import router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
